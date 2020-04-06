a9ff5d825d3b16d72a40c1c347ff2f2f5e1dc3f0
Yurii Khomenko <yuriikhomenko@macBookProYurii.home>
2020-04-06 20:15:25 +0200

    Добавляет модули

---
+++ src/components/day.js
@@ -0,0 +1,7 @@
+export const createDayTemplate = () => {
+  return (`<li class="trip-days__item  day">
+              <div class="day__info"></div>
+              <ul class="trip-events__list">
+              </ul>
+            </li>`);
+};
